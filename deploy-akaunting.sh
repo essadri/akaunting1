873eb1d12066b3450b488e55e0c482c9801b7e35
jawadessadri123 <jawadessadri123@gmail.com>
2026-01-09 19:05:26 +0100

    deploy akaunting friday first try

--- deploy-akaunting.sh
+++ deploy-akaunting.sh
@@ -1,1 +1,7 @@
- 
+#!/bin/bash
+
+kubectl apply -f ~/deploy/k8s/akaunting-all.yaml
+
+sudo -E minikube tunnel --profile=minikube &
+
+kubectl port-forward -n akaunting svc/akaunting 8080:80 --address 0.0.0.0 &
