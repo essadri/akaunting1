1aa4446d18463b4045ee3c7de7805a533b0a1f93
jawadessadri123 <jawadessadri123@gmail.com>
2026-01-09 20:25:10 +0100

    make tunnel and port-forward idempotent 8

--- deploy-akaunting.sh
+++ deploy-akaunting.sh
@@ -6,18 +6,18 @@ set -e
 kubectl apply -f ~/deploy/k8s/akaunting-all.yaml
 
 # Wait for deployments to be ready
-sleep 70 
+sleep 100
 
 # Start Minikube tunnel if not already running
-if ! pgrep -f "minikube tunnel" >/dev/null; then
-  sudo -E minikube tunnel --profile=minikube &
-fi
+
+sudo -E minikube tunnel --profile=minikube &
+
 
 # Wait a bit for services
 sleep 5
 
 # Start port-forward only if not already running
-if ! pgrep -f "kubectl port-forward -n akaunting svc/akaunting 8080:80" >/dev/null; then
-  kubectl port-forward -n akaunting svc/akaunting 8080:80 --address 0.0.0.0 & 
-fi
+
+kubectl port-forward -n akaunting svc/akaunting 8080:80 --address 0.0.0.0 & 
+
 
